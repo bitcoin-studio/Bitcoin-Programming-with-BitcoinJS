72132a6c1aca78a27030dda7627c822dc902ddc8
Janaka-Steph <rochestephane@yahoo.fr>
2019-11-16 13:48:48 +0100

    swap code

--- code/P2WSH_swap_on2off_funding.js
+++ code/P2WSH_swap_on2off_funding.js
@@ -11,7 +11,7 @@ const PAYMENT_HASH = ''
 const swapContractGenerator = function(claimPublicKey, refundPublicKey, preimageHash, cltv) {
   return bitcoin.script.compile([
     bitcoin.opcodes.OP_HASH160,
-    bitcoin.crypto.ripemd160(PAYMENT_HASH), // HASH160 of invoice payment hash
+    bitcoin.crypto.ripemd160(Buffer.from(PAYMENT_HASH, 'hex')),
     bitcoin.opcodes.OP_EQUAL,
     bitcoin.opcodes.OP_IF,
     claimPublicKey,
@@ -33,11 +33,18 @@ const keyPairSwapProvider = bitcoin.ECPair.fromWIF(bob[1].wif, network)
 const timelock = bip65.encode({ blocks: TIMELOCK })
 console.log('Timelock expressed in block height:')
 console.log(timelock)
+console.log()
 
 // Generate swap contract
 const swapContract =  swapContractGenerator(keyPairSwapProvider.publicKey, keyPairUser.publicKey, PAYMENT_HASH, timelock)
 console.log('Swap contract (witness script):')
 console.log(swapContract.toString('hex'))
+console.log()
+
+// Swap contract's SHA256 hash
+console.log('Swap contract\'s SHA256 hash:')
+console.log(bitcoin.crypto.sha256(swapContract).toString('hex'))
+console.log()
 
 // Generate swap contract bitcoin address
 const p2wsh = bitcoin.payments.p2wsh({redeem: {output: swapContract, network}, network})
